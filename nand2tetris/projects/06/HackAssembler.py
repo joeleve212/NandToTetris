0042c00fd4f438deb348f8a234bfbd1c7f4d0213
Joe Leveille <joeleve212@gmail.com>
2020-06-15 13:06:14 -0500

    Read in from asm file to list of strings

--- nand2tetris/projects/06/HackAssembler.py
+++ nand2tetris/projects/06/HackAssembler.py
@@ -1,2 +1,16 @@
 # NAND -> Tetris Assembler - Joe Leveille
 
+import sys
+inFileName = sys.argv[1]         #Use first command line arg as name of input file
+inFile = open(inFileName,"r")     
+lines = inFile.readlines()      #populate lines with strings from each line
+for i in range(len(lines)):     #scroll through each line of file
+    if(i!=len(lines)-1):        #if not last line,
+        lines[i] = lines[i][0:len(lines[i])-1] #remove newline from each line
+    lines[i] = str(lines[i]).replace(" ","")    #removes whitespace within line
+inFile.close()  #close file - all data is in lines
+
+
+print(lines[0])
+print(lines[1])
+print(lines[2])
